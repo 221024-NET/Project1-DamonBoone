575764f6fc2fa7c41804b72e6f647ce8837309f0
Damon <damonboone99@gmail.com>
2022-11-11 14:54:54 -0800

    add ticket repository class files

---
+++ Project1.Data/TicketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Project1.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Data
+{
+    internal interface TicketRepository
+    {
+        public bool createTicket(double amount, string description);
+        public Ticket getAllTickets(int id);
+    }
+}
---
+++ Project1.Data/TicketSqlRepository.cs
@@ -0,0 +1,24 @@
+﻿using Project1.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Data
+{
+    public class TicketSqlRepository : TicketRepository
+    {
+        string connectionString = File.ReadAllText("F:/Revature/Project1/connectionString.txt");
+
+        public bool createTicket(double amount, string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket getAllTickets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
