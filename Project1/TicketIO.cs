f31ad5f5cdbc1041eb083d9abaf8257c29c7b30d
Damon <damonboone99@gmail.com>
2022-11-12 13:16:48 -0800

    add TicketIO and TicketManager classes

---
+++ Project1/TicketIO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class TicketIO
+    {
+        public TicketIO() { }
+
+        public double getAmount()
+        {
+            double amount = 0;
+
+            Console.WriteLine("Please enter the amount in USD: ");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            return amount;
+        }
+    }
+}
---
+++ Project1/TicketManager.cs
@@ -0,0 +1,19 @@
+﻿using Project1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.App
+{
+    public class TicketManager
+    {
+        TicketRepository repo;
+
+        public TicketManager(TicketRepository repo)
+        {
+            this.repo = repo;
+        }
+    }
+}
